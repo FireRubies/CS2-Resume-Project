a7daed9151f88fe6182927cfc8434c8fb63e7ae8
FireRubies <40702001+FireRubies@users.noreply.github.com>
2022-10-03 11:18:00 -0700

    more comments

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//Functions for light/dark mode implementation.
+
+//Function to swap to specified stylesheet
 function swapStyleSheet(sheet) {
     document.getElementById("pagestyle").setAttribute("href", sheet);
 }
 
-//document.cookie = "username=John Doe";
-
+//Function to set a specific cookie
 function setCookie(cname, cvalue, exdays) {
   const d = new Date();
   d.setTime(d.getTime() + (exdays*24*60*60*1000));
@@ -12,6 +14,7 @@ function setCookie(cname, cvalue, exdays) {
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+//Function to get specified cookie
 function getCookie(cname) {
   let name = cname + "=";
   let decodedCookie = decodeURIComponent(document.cookie);
@@ -28,12 +31,14 @@ function getCookie(cname) {
   return "";
 }
 
+//Function to swap to the specified mode.
 function set_mode(mode) {
   stylesheet = "stylesheets/" + mode + ".css"
   console.log('Swapping to: ', stylesheet);
   swapStyleSheet(stylesheet);
 }
 
+//Function to check if the "mode" cookie is already set.
 function checkCookie() {
   let mode = getCookie("mode");
   if (mode != "" && mode != null) {
@@ -48,6 +53,8 @@ function checkCookie() {
   }
 }
 
+
+//Switches mode based on the value of the cookie "mode"
 function switch_mode() {
   if (getCookie('mode') == 'light_mode') {
     setCookie('mode', 'dark_mode');
